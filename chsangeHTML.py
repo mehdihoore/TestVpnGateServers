535355d3ff2753de9d311dc09f28b498e02bbb4e
m.he <49630235+mehdihoore@users.noreply.github.com>
2023-08-05 17:06:02 +0330

    Update chsangeHTML.py

--- chsangeHTML.py
+++ chsangeHTML.py
@@ -106,6 +106,7 @@ html_content = f'''
 <body>
     <h1>SSTP SERVERS @ {persian_now}</h1>
     <h2>Username: <font color:"red">vpn</font> | Password: <font color:"red">vpn</font></h2>
+    <h3><font color:"pink">سرورها هر یک ساعت آپدیت می شوند</font></h3>
     <select id="filterCountry" onchange="filterTable()">
         {''.join([f'<option value="{country}">{country}</option>' for country in country_filter])}
     </select>
